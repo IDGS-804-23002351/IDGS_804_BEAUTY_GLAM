fb4e3c7d135c3cee1349e689a3389884f28469f7
Jimena Oropeza Cruces <oropezajim11@gmail.com>
2026-04-17 14:09:40 -0600

    Ultimas modificaciones 17/04/2026

--- config.py
+++ config.py
@@ -10,10 +10,7 @@ class Config:
     PERMANENT_SESSION_LIFETIME = 600 
     SECURITY_TOKEN_MAX_AGE = 600
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Junior021205$$$@localhost/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://LAPLANTA:LAPLANTA10s.@localhost/salon_belleza'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://jimena:jimena123.@localhost/salon_belleza'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MONGO_URI = "mongodb://localhost:27017/salon_belleza"
@@ -31,9 +28,5 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root1234@127.0.0.1/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Junior021205$$$@127.0.0.1/salon_belleza'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://LAPLANTA:LAPLANTA10s.@localhost/salon_belleza'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://jimena:jimena123.@localhost/salon_belleza'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
